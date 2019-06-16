edb3c7027d71557f956e6ee8a752f0b51afd5345
Issei Horie <is2ei.horie@gmail.com>
2019-06-16 11:04:24 +0900

    Added files

---
+++ post-message.js
@@ -0,0 +1,11 @@
+const config = require('./config');
+const Typetalk = require('typetalk');
+
+const typetalk = new Typetalk.Client({
+  token: config.TYPETALK_TOKEN
+});
+
+const id = config.TYPETALK_TOPIC_ID;
+typetalk.postMessage({message: "Hello, World!"}, {id})
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
